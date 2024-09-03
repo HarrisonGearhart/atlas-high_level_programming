7045a4ea0a7932eb04f3c4961df7b6ec586cbeb1
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2024-09-02 23:45:50 -0500

    file_12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,21 @@
+def roman_to_int(roman_string):
+	if not roman_string or roman_string is None
+		return 0
+
+	roman_dictionary = {
+		"I": 1,
+		"V": 5,
+		"X": 10, 
+		"L": 50,
+		"C": 100,
+		"D": 500,
+		"M": 1000,
+	}
+	num = 0
+
+	for x in range(len(roman_string)):
+		if roman_dictionary.get(roman_string[x], 0) == 0:
+			return 0
+		else:
+			num += roman_dictionary[roman_string[x]]
+	return (num)
