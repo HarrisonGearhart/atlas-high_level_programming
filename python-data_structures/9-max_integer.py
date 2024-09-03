ea276fa0eb71af944c017bb3b7ce70251d881e32
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2024-09-02 22:19:36 -0500

    file_9 forgot else statement line 6

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,9 +3,10 @@ def max_integer(my_list=[]):
 	if len(my_list) == 0:
 		return "None"
 
-	max = my_list[0]
-	for i in range(len(my_list)):
-		if my_list[i] > max:
-			my_list[i] = max
+	else:
+		max = my_list[0]
+		for i in range(len(my_list)):
+			if my_list[i] > max:
+				my_list[i] = max
 
-	return max
+		return max
