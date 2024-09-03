2bf1b49d074b47ab2fb8bc48c9c82b03bfe4ffa0
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2024-09-03 12:58:27 -0500

    file_0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
 	count = 0
-	for i in my_list[:x]:
+	for i in range(0, x):
 		try:
-			print("{}".format(i), end="")
-			count += 1
+			print("{}".format(my_list[i]), end="")
 		except:
-			pass
+			break
+		else:
+			count
 		print()
 		return count
