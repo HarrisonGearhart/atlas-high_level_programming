67605bc19d41853ce049ae4ee1382205ff9d68ca
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2024-09-03 13:15:51 -0500

    file_2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
 	for i in range(0, x):
 		try:
 			print("{:d}".format(my_list[i]), end="")
-			ret += x
+			count += x
 		except (ValueError, TypeError):
 			continue
 		print("")
