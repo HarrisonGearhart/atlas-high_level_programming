fe3d1b046ca96f9889a726159415605415a41397
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2024-09-03 12:04:38 -0500

    file_0 change formatted print

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
 	count = 0
-	for i in range(x):
+	for i in my_list[:x]:
 		try:
-			print("{}".format(my_list[i]), end="")
+			print("{}".format(i), end="")
 			count += 1
-		except IndexError:
-			break
-		print("")
+		except:
+			pass
+		print()
 		return count
