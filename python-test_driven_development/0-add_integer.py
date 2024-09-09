0950196197a37598fc8f64dd1befb5d1df20a513
Harrison  Gearhart <harrisongearhart@Harrisons-MacBook-Air.local>
2024-09-09 14:57:42 -0500

    file_0 add documentation

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -2,9 +2,9 @@
 """This function adds two integers"""
 
 def add_integer(a, b=98):
-"""if a or b isn't a float or integer, raise error
-cast floats into ints
-return a + b"""
+	"""if a or b isn't a float or integer, raise error
+	cast floats into ints
+	return a + b"""
 	if a != int or a != float:
 		raise TypeError("a must be an integer")
 	if b != int or a != float:
