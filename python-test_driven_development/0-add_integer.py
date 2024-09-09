5809e96379c00612e845e6e34088262e694e440b
Harrison  Gearhart <harrisongearhart@Harrisons-MacBook-Air.local>
2024-09-09 15:03:39 -0500

    file_0 re-order float casts

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -9,9 +9,9 @@ def add_integer(a, b=98):
 		a = int(a)
 	if b == float:
 		b = int(b)
-	if a != int
+	if a != int:
 		raise TypeError("a must be an integer")
-	if b != int
+	if b != int:
 		raise TypeError("b must be an integer")
 	else:
 		return a + b
