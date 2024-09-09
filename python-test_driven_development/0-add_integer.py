5917119642c3ef5e639988ad3b17197b40613467
Harrison  Gearhart <harrisongearhart@Harrisons-MacBook-Air.local>
2024-09-09 15:02:59 -0500

    file_0 re-order float casts

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,11 +5,13 @@ def add_integer(a, b=98):
 	"""if a or b isn't a float or integer, raise error
 	cast floats into ints
 	return a + b"""
-	if a != int or a != float:
+	if a == float:
+		a = int(a)
+	if b == float:
+		b = int(b)
+	if a != int
 		raise TypeError("a must be an integer")
-	if b != int or a != float:
+	if b != int
 		raise TypeError("b must be an integer")
 	else:
-		a = int(a)
-		b = int(b)
 		return a + b
