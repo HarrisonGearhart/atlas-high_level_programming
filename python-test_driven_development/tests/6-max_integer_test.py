556821c466e37a3b76d5d3fa0bab3dcc4575e17b
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2024-09-10 14:25:02 -0500

    test_6

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+#6-max_integer_test.py
+"""Unit test for max integer"""
+
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+	def test_max(self):
+		self.assertAlmostEqual(max_integer([1, 2, 3]), 3)
+		self.assertAlmostEqual(max_integer([25, 50, 100, 75]), 100)
+		self.assertAlmostEqual(max_integer([-1, -2, -3]), -1)
+		self.assertAlmostEqual(max_integer([]), None)
+
+	def test_non_int(self):
+		self.assertAlmostEqual(TypeError, max_integer, True)
