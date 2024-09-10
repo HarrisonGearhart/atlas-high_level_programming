6c7e97b46c72b616aee6ab7f77fc8fa34f27809b
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2024-09-10 12:02:17 -0500

    file_5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/python3
 """function that prints a text with 2 new lines after each of these characters: ., ? and :"""
 def text_indentation(text):
 	"""text must be a string
