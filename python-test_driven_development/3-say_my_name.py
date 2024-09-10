4e9c0fec200e13488aa78c73c42a62a76e3ad8aa
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2024-09-10 11:01:12 -0500

    file_3

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
+"""This function prints "My name is <first name> <last name>" with user inputted names."""
+
 def say_my_name(first_name, last_name=""):
+	"""first_name and last_name must be a string"""
+	if type(first_name) is not str:
+		raise TypeError("first_name must be a string")
+	if type(last_name) is not str:
+		raise TypeError("last_name must be a string")
+	else:
+		print("My name is {} {}", (first_name, last_name))
