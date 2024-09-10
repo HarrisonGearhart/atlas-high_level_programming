d6d47a766cc8c7d97a448c6fb494eada57779b10
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2024-09-10 12:01:41 -0500

    file_5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,2 +1,13 @@
-
+#!/usr/bin/bash
+"""function that prints a text with 2 new lines after each of these characters: ., ? and :"""
 def text_indentation(text):
+	"""text must be a string
+	adds 2 blank lines after delimeters
+	no space at the beginning or at the end of each printed line
+	splits the lines of text between each delimeter"
+	"""
+	if type(text) is not str:
+		raise TypeError("text must be a string")
+	for delimeter in ".:?":
+		text = (delimeter + "\n\n").join([line.strip(" ") for line in text.split(delimeter)])
+	print("{}".format(text), end="")
