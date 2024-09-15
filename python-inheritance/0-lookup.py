4627fa91cca49a48886243c374f4bb7bd0d2c507
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2024-09-15 10:30:38 -0500

    file_0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 """Defines a lookup function"""
+
+
 def lookup(obj):
-	"""returns the list of available attributes and methods of an object"""
-	return dir(obj)
+    """returns the list of available attributes and methods of an object"""
+    return dir(obj)
+    
