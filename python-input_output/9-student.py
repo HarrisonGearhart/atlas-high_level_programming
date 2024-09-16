cd80105b0136f7e3fde40c667289b89e2ec3ac6f
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2024-09-16 16:45:03 -0500

    file_8

---
+++ python-input_output/9-student.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Class Student"""
+
+class Student:
+    """Attributes first, last, age
+    method that retrieve dictionary representation of Student"""
+    def __init__ (self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """dictionary method"""
+        return self.__dict__
