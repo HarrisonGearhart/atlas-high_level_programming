f9721b1720ef70b4f512d133091aec21d9e3fd67
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2024-09-16 16:21:54 -0500

    file_5

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""function that writes an Object to a text file, using a JSON representation
+"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """save object to json file"""
+    with open(filename, "w") as json_file:
+        json.dmp(my_obj, json_file)
