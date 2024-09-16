8dc7dad91300ed0f21fb58d647be7205503cb099
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2024-09-16 15:57:28 -0500

    file_1 spaces

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,6 +3,6 @@
 
 
 def read_file(filename=""):
-	"""reads text file and prints it"""
-	with open(filename, encoding="utf-8") as file:
-		print(file.read(), end='')
+    """reads text file and prints it"""
+    with open(filename, encoding="utf-8") as file:
+        print(file.read(), end='')
