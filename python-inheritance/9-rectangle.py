d7aba21025b7674210f050ee2915f77252705353
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2024-09-16 15:28:36 -0500

    file_9 indentation

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -18,10 +18,9 @@ class BaseGeometry:
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
 
+
 class Rectangle(BaseGeometry):
     """Rectangle class that inherits from BaseGeometry"""
-
-
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.integer_validator("height", height)
