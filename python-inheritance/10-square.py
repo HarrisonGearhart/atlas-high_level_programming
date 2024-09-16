262c8c978e3cc90bd4eb3cb89274997d02a74855
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2024-09-16 15:39:47 -0500

    file_10 spaces

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -5,11 +5,11 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-	"Class Square inhereting from Rectangle"
-	def __init__(self, size):
-		self.integer_validator("size", size)
-		self.__size = size
-		super().__init__(size, size)
+    "Class Square inhereting from Rectangle"
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
 
-	def area(self):
-		return self.__size ** 2
+    def area(self):
+        return self.__size ** 2
