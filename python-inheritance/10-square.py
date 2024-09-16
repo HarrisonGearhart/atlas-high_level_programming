35105ccc2d39b4a5da9f5855067b0c6d896c7c24
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2024-09-16 15:36:35 -0500

    file_10

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Import BaseGeometry Class and Rectangle Subclass"""
+
+Rectangle = __import__('9-Rectangle').Rectangle
+
+
+class Square(Rectangle):
+	"Class Square inhereting from Rectangle"
+	def __init__(self, size):
+		self.integer_validator("size", size)
+		self.__size = size
+		super().__init__(size, size)
+
+	def area(self):
+		return self.__size ** 2
