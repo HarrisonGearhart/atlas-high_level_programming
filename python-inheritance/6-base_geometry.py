5a555444ff01c1643b829fc79ffed733278cc3ef
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2024-09-16 14:54:04 -0500

    file_6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Create an class BaseGeometry"""
+
+
+class BaseGeometry:
+    """BaseGeometry class
+    attribute area
+    raise exception is not emplemented"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    
