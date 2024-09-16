8aec16c3085a3824974bfa1ad23148f54686f87e
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2024-09-16 14:33:58 -0500

    file_2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Defines function that checks if the object is exactly an instance of the specified class"""
+"""Defines function that checks 
+if the object is exactly an instance of the specified class"""
 
 
 def is_same_class(obj, a_class):
