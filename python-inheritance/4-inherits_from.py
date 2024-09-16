946080c87c7e91f5fe630cc01c84d8c8ec2a91b5
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2024-09-16 14:44:12 -0500

    file_4

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Checks if the object is an instance of a class that
+inherited (directly or indirectly) from the specified class"""
+def inherits_from(obj, a_class):
+    """Checks if object type is a_class
+    if so, Returns true if is instance of a class is inherented
+    false otherwise"""
+        if type(object) != a_class:
+            return issubclass(type(opject), a_class)
+        else:
+            return False
