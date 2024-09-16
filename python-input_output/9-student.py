f296d5efd36baa698a5dd681f727b6af91eacc62
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2024-09-16 16:45:37 -0500

    file_8

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """Class Student"""
 
+
 class Student:
     """Attributes first, last, age
     method that retrieve dictionary representation of Student"""
-    def __init__ (self, first_name, last_name, age):
+    def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
