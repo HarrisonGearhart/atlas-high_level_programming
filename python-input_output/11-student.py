e53a832d4ddcea8f67606cd41ca7d40f9dd8b470
Harrison  Gearhart <harrisongearhart@Harrisons-MacBook-Air.local>
2024-09-17 13:01:12 -0500

    file_11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Class Student"""
+
+
+class Student:
+    """Attributes first, last, age
+    method that retrieve dictionary representation of Student
+    If attrs is a list of strings
+    only attribute names contained in this list must be retrieved
+	method that replaces all attributes of the Student instance:"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        obj = self.__dict__.copy()
+        if type(attrs) is list:
+            for item in attrs:
+                if type(item) is not str:
+                    return obj
+
+            d_list = {}
+
+            for x in range(len(attrs)):
+                for y in obj:
+                    if attrs[x] == y:
+                        d_list[y] = obj[y]
+            return d_list
+
+        return obj
+
+	def reload_from_json(self, json):
+		for attr in json:
+			self.__dict__[attr] = json[attr]
