2d8257b9e845bb499b6521f90dff5f10ed778846
Harrison  Gearhart <harrisongearhart@Harrisons-MacBook-Air.local>
2024-09-17 12:45:05 -0500

    file_10

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -5,16 +5,16 @@
 class Student:
     """Attributes first, last, age
     method that retrieve dictionary representation of Student
-	If attrs is a list of strings
-	only attribute names contained in this list must be retrieved."""
+    If attrs is a list of strings
+    only attribute names contained in this list must be retrieved."""
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-	def to_json(self, attrs=None):
-		if type(attrs) is list:
-			for item in attrs:
-				if type(item) is not str:
-					return obj
-	
+    def to_json(self, attrs=None):
+        if type(attrs) is list:
+            for item in attrs:
+                if type(item) is not str:
+                    return obj
+    
