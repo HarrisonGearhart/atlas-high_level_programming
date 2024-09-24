40a2de0ba503883a3c26845f5821c4578587095a
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2024-09-24 14:34:42 -0500

    Square(Rectangle) update method

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -20,11 +20,31 @@ class Square(Rectangle):
 
 	def update(self, *args, **kwargs):
 		"""update args"""
-		attr = ["id", "size", "x", "y"]
-		if args:
-			for att, num in zip(attr, args):
-				setattr(self, att, num)
-		elif kwargs:
-			for key, value in kwargs.items():
-				if key in attr:
-					setattr(self, key, value)
+		if args and len(args) != 0:
+			a = 0
+			for arg in args:
+				if a == 0:
+					if arg is None:
+						self.__init__(self.size, self.x, self.y)
+					else:
+						self.id = arg
+				elif a == 1:
+					self.size = arg
+				elif a == 2:
+					self.x = arg
+				elif a == 3:
+					self.y = arg
+				a += 1
+		elif kwargs and len(kwargs) != 0:
+			for key, value, in kwargs.items():
+				if key == "id":
+					if value is None:
+						self.__init__(self.size, self.x, self.y)
+					else:
+						self.id = value
+				elif key == "size":
+					self.size = value
+				elif key == "x":
+					self.x = value
+				elif key == "y":
+					self.y = value
