8bd69b5932daf21d16546103b64c881ba3986f8f
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2024-09-24 13:19:45 -0500

    Rectangle(Base) typeerrors

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -48,7 +48,7 @@ class Rectangle(Base):
     def x(self, value):
         if type(value) != int:
             raise TypeError("x must be an integer")
-        if value <= 0:
+        if value < 0:
             raise TypeError("x must be >= 0")
         self.__x = value
 
@@ -60,6 +60,6 @@ class Rectangle(Base):
     def y(self, value):
         if type(value) != int:
             raise TypeError("y must be an integer")
-        if value <= 0:
+        if value < 0:
             raise TypeError("y must be >= 0")
         self.__y = value
