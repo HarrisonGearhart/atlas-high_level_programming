8d3192012ab973432f734d16f9a4b9833d507205
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2024-09-24 12:51:02 -0500

    Base class __nb_objects

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""Base Class Model"""
+
+
+class Base:
+    """Base model for all other classes
+    __nb_onjects attribute
+    """
+    __nb_objects = 0
+    
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
