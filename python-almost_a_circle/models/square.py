54aade3871c5117c771dafd91b0ce484013b1eeb
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2024-09-24 14:13:51 -0500

    Square(Rectangle) model

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Define a Sqaure Class that inherits from Rectangle model"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+	"""Sqaure class inheriting from Rectangle"""
+	def __init__(self, size, x=0, y=0, id=None):
+		super().__init__(size, size, x, y, id)
+		self.__size = size
+
+	@property
+	def size(self):
+		return self.__size
+
+	@size.setter
+	def size(self, value):
+		self.width = value
+		self.height = value
