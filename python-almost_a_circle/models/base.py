420b554fac4330e7cb2238e77cb8c28f05af0050
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2024-09-24 16:38:30 -0500

    Base static method

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -14,3 +14,12 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return JSON serialization of a list of dicts.
+        args - list of dictionaries
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
