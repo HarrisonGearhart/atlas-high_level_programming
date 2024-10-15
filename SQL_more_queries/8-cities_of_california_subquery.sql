e15c5c7368ea35accbaa15bb8829c20389d3a0d3
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2024-10-15 16:13:08 -0500

    file_8 restructure

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,2 @@
 -- lists all the cities of California that can be found in the database hbtn_0d_usa.
-SELECT cities.id, cities.name
-FROM cities, states
-WHERE cities.state.id = states.id AND states.name = 'California'
-ORDER BY cities.id ASC;
+SELECT cities.id, cities.name FROM cities, states WHERE cities.state.id = states.id AND states.name = 'California' ORDER BY cities.id ASC;
