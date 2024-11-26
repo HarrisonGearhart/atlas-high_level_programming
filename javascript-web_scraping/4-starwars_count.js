4990cb8428feae351db7ec624e841a1b89b86e13
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2024-11-26 16:01:15 -0600

    task4

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (err, response, body) {
+    if (err) console.log(err);
+    else {
+        let count = 0;
+        const results = JSON.parse(body).results;
+        for (let i = 0; i < results.length; i++) {
+            const characters = results[i].characters;
+            for (let j = 0; j < characters.length; j++) {
+                if (characters[j].search('18') > 0) {
+                    count++;
+                } 
+            }
+        }
+        console.log(count);
+    }
+});
