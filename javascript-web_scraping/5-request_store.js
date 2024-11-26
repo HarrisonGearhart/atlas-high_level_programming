bf06c562db5f94873d2315f006a55f6286877d04
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2024-11-26 16:09:01 -0600

    task5 =>

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -5,10 +5,10 @@ const fs = require('fs');
 const url = process.argv[2];
 const filename = process.argv[3];
 
-request(url, (err, response, body) {
+request(url, (err, response, body) => {
     if (err) console.log(err);
     else {
-        fs.writeFile(filename, body, 'utf8', (err) {
+        fs.writeFile(filename, body, 'utf8', (err) => {
             if (err) console.log(err);
         });
     }
